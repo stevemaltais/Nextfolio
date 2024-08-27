b8ca7b0c904e76e6f946889ef6b763357084bebd
Steve Maltais <stevemaltais@icloud.com>
2024-08-27 12:25:48 -0400

    Test

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,9 @@
-// Dans index.js ou là où vous voulez utiliser le Carousel
 import React from 'react';
 
-import ImageOverlay from "@/components/HomePageComponents/ImageOverlay";
-import HomeSection from "@/components/HomePageComponents/HomeSection";
-import AboutSection from "@/components/HomePageComponents/AboutSection";
-import CompetenceSection from "@/components/HomePageComponents/CompetenceSection";
-import ContactSection from '@/components/HomePageComponents/ContactSection';
-
-import { getStaticProps as getProjetsStaticProps } from '@/graphql/queries';
-import { PorteFolioSection } from '@/components/HomePageComponents/PorteFolioSection'; 
-
-export default function Home({ projets }) {
-
+const IndexPage = () => {
   return (
-    < >
-    
-      <ImageOverlay />
-      <HomeSection />
-      <AboutSection />
-      <CompetenceSection />
-      <PorteFolioSection  projets={projets} options={{ loop: true }} />
-      <ContactSection/>
-      
-    </>
+    <div>Bienvenue sur mon portfolio!</div>
   );
-}
+};
 
-export const getStaticProps = getProjetsStaticProps;
+export default IndexPage;
