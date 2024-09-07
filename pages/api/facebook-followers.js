44898708fcf74708de56ddbd54a9f16edb3f7c17
Steve Maltais <stevemaltais@icloud.com>
2024-09-07 17:03:20 -0400

    Facebook API

--- pages/api/facebook-followers.js
+++ pages/api/facebook-followers.js
@@ -3,11 +3,11 @@ import axios from 'axios';
 export default async function handler(req, res) {
   try {
     const response = await axios.get(
-      `https://graph.facebook.com/v14.0/me?fields=followers_count&access_token=${process.env.NEXT_PUBLIC_FACEBOOK_ACCESS_TOKEN}`
+      `https://graph.facebook.com/v14.0/${process.env.NEXT_PUBLIC_FACEBOOK_APP_ID}?fields=followers_count&access_token=${process.env.NEXT_PUBLIC_FACEBOOK_ACCESS_TOKEN}`
     );
     res.status(200).json({ followers: response.data.followers_count });
   } catch (error) {
-    console.error('Erreur Facebook API:', error.response.data);
+    console.error('Erreur Facebook API:', error.response ? error.response.data : error.message);
     res.status(500).json({ error: 'Erreur lors de la récupération des followers' });
   }
 }
