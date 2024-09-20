449893d32414874dfe48e4b8047f757f2a1b1324
Steve Maltais <stevemaltais@icloud.com>
2024-09-20 11:27:58 -0400

    Detail de projet1

--- components/UI/Drawer.js
+++ components/UI/Drawer.js
@@ -1,7 +1,22 @@
-import React from 'react';
-import styles from '@/components/UI/Drawer.module.scss'; // Nous allons créer ce fichier SCSS pour le style
+import React, { useEffect } from 'react';
+import styles from '@/components/UI/Drawer.module.scss';
 
 const Drawer = ({ isOpen, onClose, children }) => {
+  
+  // Utilise useEffect pour bloquer/débloquer le scroll
+  useEffect(() => {
+    if (isOpen) {
+      document.body.classList.add('no-scroll');
+    } else {
+      document.body.classList.remove('no-scroll');
+    }
+
+    // Nettoyage de l'effet quand le composant est démonté ou le drawer est fermé
+    return () => {
+      document.body.classList.remove('no-scroll');
+    };
+  }, [isOpen]);
+
   return (
     <div className={`${styles.drawer} ${isOpen ? styles.open : ''}`}>
       <div className={styles.drawer__content}>
