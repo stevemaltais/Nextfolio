0492a6b915ed8a6adcd061eeab50db2f4de29afd
Steve Maltais <stevemaltais@icloud.com>
2024-09-20 11:29:56 -0400

    Detail de projet1

--- components/UI/Drawer.js
+++ components/UI/Drawer.js
@@ -1,8 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from '@/components/UI/Drawer.module.scss';
 
 const Drawer = ({ isOpen, onClose, children }) => {
-  
+  const [touchStartX, setTouchStartX] = useState(0);
+  const [touchEndX, setTouchEndX] = useState(0);
+
   // Utilise useEffect pour bloquer/débloquer le scroll
   useEffect(() => {
     if (isOpen) {
@@ -17,8 +19,34 @@ const Drawer = ({ isOpen, onClose, children }) => {
     };
   }, [isOpen]);
 
+  // Fonction pour gérer le début du swipe (touchstart)
+  const handleTouchStart = (e) => {
+    setTouchStartX(e.touches[0].clientX);
+  };
+
+  // Fonction pour gérer la fin du swipe (touchend)
+  const handleTouchMove = (e) => {
+    setTouchEndX(e.touches[0].clientX);
+  };
+
+  // Fonction pour détecter le swipe
+  const handleTouchEnd = () => {
+    if (touchStartX - touchEndX > 100) {
+      // Si on swipes vers la gauche (drawer à droite), on ferme
+      onClose();
+    }
+    // Réinitialisation des positions
+    setTouchStartX(0);
+    setTouchEndX(0);
+  };
+
   return (
-    <div className={`${styles.drawer} ${isOpen ? styles.open : ''}`}>
+    <div
+      className={`${styles.drawer} ${isOpen ? styles.open : ''}`}
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
+      onTouchEnd={handleTouchEnd}
+    >
       <div className={styles.drawer__content}>
         <button className={styles.drawer__closeButton} onClick={onClose}>
           ×
