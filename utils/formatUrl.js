6242a7fdc27bfb26f8692bc28221c5fa43f4dbb4
Steve Maltais <stevemaltais@icloud.com>
2024-09-27 16:34:53 -0400

    SEO INTEGRATION111

--- utils/formatUrl.js
+++ utils/formatUrl.js
@@ -1,6 +1,7 @@
 // utils/formatUrl.js
+// export const formatUrl = (url) => {
+//     if (!url) return '';
 export const formatUrl = (url) => {
     if (!url) return '';
     return url.replace(/^https?:\/\//, '').replace(/\/$/, '');
   };
-  
