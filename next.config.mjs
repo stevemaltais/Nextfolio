66031b06ed01653c1f5102f9dc2e20ae9dbde8fb
Steve Maltais <stevemaltais@icloud.com>
2024-09-27 16:40:35 -0400

    SEO INTEGRATION111

--- next.config.mjs
+++ next.config.mjs
@@ -19,16 +19,17 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: '/:path*',  // Redirige toutes les pages
+        source: '/:path*',
         has: [
           {
             type: 'host',
-            value: 'stevemaltais.dev',  // Redirige si le domaine est sans "www"
+            value: 'stevemaltais.dev',  // Seulement rediriger si l'URL est sans "www"
           },
         ],
         destination: 'https://www.stevemaltais.dev/:path*',  // Destination avec "www"
         permanent: true,  // Redirection 301
       },
+      // Assurez-vous que le domaine avec "www" ne soit pas redirigé à nouveau
     ];
   },
 };
